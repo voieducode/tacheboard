b1d05a2afadde1e325f330b7909da7b2786e1c13
Dinh Doan Van Bien <dinh@doanvanbien.com>
2012-05-17 23:05:46 +0100

    renaming to TacheBoard

---
+++ Tests/RouteTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TacheBoard.Web;
+using Xunit;
+using TacheBoard;
+
+namespace TacheBoard.Tests
+{
+    public class RouteTests
+    {
+        [Fact]
+        public void TestIncomingRoutes()
+        {
+            var tester = new MvcRouteUnitTester.RouteTester<MvcApplication>();
+        }
+    }
+}
---
+++ Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TacheBoard.Web.MvcApplication" Language="C#" %>
